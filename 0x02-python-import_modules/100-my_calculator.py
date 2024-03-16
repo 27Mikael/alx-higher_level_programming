28a26a25d6a2a3935c814955eb573edad25df8eb
27Mikael <vitalybodgan@gmail.com>
2024-03-16 15:13:05 +0200

    added missing files

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+
+
+def process(argv):
+    if len(sys.argv) > 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
+
+    a = int(sys.argv[1])
+    op = sys.argv[2]
+    b = int(sys.argv[3])
+
+    for argv in sys.argv[1:3]:
+        if op == "+":
+            print("{:d} + {:s} = {:d}".format(a, b, add(a, b)))
+        elif op == "-":
+            print("{:d} - {:s} = {:d}".format(a, b, sub(a, b)))
+        elif op == "*":
+            print("{:d} * {:s} = {:d}".format(a, b, mul(a, b)))
+        elif op == "/":
+            print("{:d} / {:s} = {:d}".format(a, b, div(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and / ")
+            sys.exit(1)
+
+
+if __name__ == "__main__":
+    import sys
+    process(sys.argv)
